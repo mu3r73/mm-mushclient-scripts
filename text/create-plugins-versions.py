afaf0791286a9fc2528208ffbc664abadbd1a66f
MueRTe <mu3r73@yandex.com>
2020-09-28 06:45:29 -0300

    added comments

--- text/create-plugins-versions.py
+++ text/create-plugins-versions.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import glob
 import hashlib
-import json
 import os
 import re
+
+# recreates plugins_versions.txt
+# usage: create-plugins-versions > plugins_versions.txt
+
 
 base_url = 'https://raw.githubusercontent.com/mu3r73/mm-mushclient-scripts/master/'
 #base_url = 'https://raw.githubusercontent.com/MateriaMagicaLLC/mm-mushclient-scripts/master/'
@@ -51,11 +54,11 @@ def find_plugin_details(filepath):
   details = {
     'aux_files': []
   }
-  with open(filepath, 'r', encoding='ISO-8859-1') as f:
+  with open(filepath, 'r', encoding = 'ISO-8859-1') as f:
     for line in f:
       ids = re_id.findall(line)
       if (ids):
-        details['id'] = ids[0]
+        details['id'] = ids[0] # 1st result
         continue
       
       if (re_begin_updates.findall(line)):
@@ -68,15 +71,15 @@ def find_plugin_details(filepath):
       url_dest_data = re_url_dest.findall(line)
       if (url_dest_data):
         details.get('aux_files').append({
-          'name': url_dest_data[0][1],
-          'dest': url_dest_data[0][2],
+          'name': url_dest_data[0][1], # 1st result, 2nd elem of tuple
+          'dest': url_dest_data[0][2], # 1st result, 3rd elem of tuple
         })
         continue
       
       url_data = re_url.findall(line)
       if (url_data):
         details.get('aux_files').append({
-          'name': url_data[0][1],
+          'name': url_data[0][1], # 1st result, 2nd elem of tuple
         })
         continue
 
@@ -117,3 +120,54 @@ def show_other_info(details):
 
 if __name__ == "__main__":
   create_plugins_versions()
+
+
+
+# examples:
+#
+# * no aux files: affects_by_name
+# 
+# ... no plugin_update_aux_url() function
+# 
+# output for plugins_versions.txt:
+# id = 6010f6bda5ebf2210998613d  hash = <blah>
+# 
+# 
+# * single aux file, no destination: virtual_arrows
+# 
+# function plugin_update_aux_url()
+#   local t = {
+#     "https://raw.githubusercontent.com/<blah>/mm-mushclient-scripts/master/res/buttons.png",
+#   }
+#   return (table.concat(t, ";"))
+# end
+# 
+# output for plugins_versions.txt:
+# id = 3c56fcf3317c6230888de0f6  hash = <blah>  aux_files = {   [1] = {     name = "buttons.png",     },   }
+# 
+#
+# * single aux file with destination: MM_GMCP_Mapper
+# 
+# function plugin_update_aux_url()
+#   local t = {
+#     "https://raw.githubusercontent.com/<blah>/mm-mushclient-scripts/master/src/mm_mapper.lua,MUSH/lua",
+#   }
+#   return (table.concat(t, ";"))
+# end
+# 
+# output for plugins_versions.txt:
+# id = f973af093e715dece34dc25f  hash = <blah>  aux_files = {   [1] = {     name = "mm_mapper.lua",     dest = "MUSH/lua",     },   }
+# 
+#
+# * several aux files: capture2dworld
+# 
+# function plugin_update_aux_url()
+#   local t = {
+#     "https://raw.githubusercontent.com/<blah>/mm-mushclient-scripts/master/res/captures.MCL,MUSH/worlds",
+#     "https://raw.githubusercontent.com/<blah>/mm-mushclient-scripts/master/res/announce.mp3,MUSH/sounds",
+#   }
+#   return (table.concat(t, ";"))
+# end
+# 
+# output for plugins_versions.txt:
+# id = b2772cad800b33a6073d9377  hash = <blah>  aux_files = {   [1] = {     name = "captures.MCL",     dest = "MUSH/worlds",     },   [2] = {     name = "announce.mp3",     dest = "MUSH/sounds",     },   }
